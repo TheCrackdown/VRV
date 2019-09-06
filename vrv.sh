120a33199fd049f7a8dbca16081c9236757b75f9
TheCrackdown <54947999+TheCrackdown@users.noreply.github.com>
2019-09-06 23:52:07 +0530

    Add execution file

---
+++ vrv.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+# The Yuki Chan
+#
+# Automated Intel-Gathering - Vulnerability Analysis - OSINT
+# Tracking - System Enumeration - And Off Course Pentesting Too
+#
+# Version : 1.0 | Codename : Waifu Sudah Lacur 
+# Coded by : Yukinoshita 47 | Garuda Security Hacker
+#
+# Tested on : Kali Linux 
+# I highly recommend using this tool by using Kali Linux OS
+# If You using another OS dont forget to install python and perl requirements
+#
+# More Info : http://www.garudasecurityhacker.org
+# 
+# This Tool Designed For Lazy Way To Pentest :)
+# Remember Educational Purpose only Not For Crime
+# Im Not Responsible If Something Bad Thing Happen
+# Use At Your Own Risk
+#
+
+echo ""
+echo "                                                   "
+echo "            ██╗    ██╗           ██╗    ██ " 
+echo "            ██║    ██║           ██║   ██╝ "
+echo "            ██║    ██║           ██╚═██╝    ██╗ "
+echo "            █████████║ ██╗  ██╗  ██ ██║     ══╝   "
+echo "                ██╔══╝ ██║  ██║  ██╔═██╗    ██╗"
+echo "                ██║    ██║  ██║  ██║   ██╗  ██║"
+echo "                ██║    ███████║  ██║    ██╗ ██║ "
+echo "                ══╝    ═══════╝  ══╝    ══╝ ══╝ "
+echo "                        The YuKi-Chan         "
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║  Automated Intel-Gathering - Vulnerability Analysis - OSINT    ║"   
+echo "║  Tracking - System Enumeration - And Off Course Pentesting Too ║"
+echo "╚════════════════════════════════════════════════════════════════╝"         
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo " ║         Version : 1.0 | Codename : Waifu Sudah Lacur         ║"
+echo "   ║    Coded by : Yukinoshita 47 | Garuda Security Hacker    ║"
+echo "    ║                 Tested on : Kali Linux                 ║"
+echo "     ║     More Info : http://www.garudasecurityhacker.org  ║"              
+echo "      ╚════════════════════════════════════════════════════╝"
+echo "         Recode The Copyright Is Not Make You A Coder Dude :p "
+echo "══════════════════════════════════════════════════════════════════"
+echo "Enter domain of your Target Below example site.com :"
+read TARGET
+echo ""
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                                                                ║"
+echo "║                           WARNING                              ║"
+echo "║                                                                ║"
+echo "║      I highly recommend using this tool by using Kali Linux OS ║"
+echo "║                                                                ║"
+echo "║      By using this tool it means you agree with terms,         ║"
+echo "║      conditions, and risks                                     ║"
+echo "║                                                                ║"
+echo "║      By using this tool you agree that                         ║"
+echo "║      1. use for legitimate security testing                    ║"
+echo "║      2. not for crime                                          ║"
+echo "║      3. the use of this tool solely for                        ║"
+echo "║         educational reasons only                               ║"
+echo "║                                                                ║"
+echo "║      By using this tool you agree that                         ║"
+echo "║      1. You are willing to be charged with criminal or state   ║"
+echo "║         law applicable by law enforcement officers             ║"
+echo "║         and government when abused                             ║"
+echo "║      2. the risk is borne by yourself                          ║"
+echo "║                                                                ║"
+echo "║         Thank you and happy pentest                            ║"
+echo "║                                                                ║"
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "█[1]█"
+echo ""
+echo ""
+echo ""
+echo "█[2]█"
+echo ""
+echo ""
+echo ""
+echo "█[3]█"
+echo ""
+echo ""
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                    YUKI-CHAN STARTED                           ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║            Let's Find Who The Hell Is This Owner               ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo ""
+echo "whois looking up (if not run maybe not installed in your OS)"
+whois $TARGET
+echo "whois looking up finished"
+echo ""
+echo "nslooking up (if not run maybe not installed in your OS)"
+nslookup $TARGET
+echo "nslooking up finished"
+echo ""
+echo "scanning with nmap (if not run maybe not installed in your OS)"
+nmap -v -O $TARGET
+echo "scanning with nmap finished"
+echo ""
+echo "starting the harvester for gathering email and subdomain information"
+python modules/theHarvester/theHarvester.py -d $TARGET -l 500 -b google
+echo "the harvester finished"
+echo ""
+echo "starting metagoofil for gathering document maybe important"
+python modules/metagoofil/metagoofil.py -d $TARGET -t doc,pdf,xls,csv,txt -l 200 -n 50 -o metagoofiles -f data.html
+echo "metagoofil finished"
+echo ""
+echo ""
+dig -x $TARGET
+python modules/sublist3r/sublist3r.py --domain $TARGET
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                    Got It :v wkwkwkwkwk                        ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo ""
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                   XSS Scanning Starting                        ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo ""
+python modules/XssPy.py -u $TARGET -v
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                   XSS Scanning Finished                        ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                   Scan Wordpress Starting                      ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo "start scanning wpscan.rb (if not run maybe not installed in your OS)"
+wpscan --url http://$TARGET --enumerate u
+echo ""
+echo "If Your OS Doesn't Have WPScan"
+echo "Dont Worry Dude I Have Alternative Scanner For You"
+echo "Next Time Use Kali Linux if you want this tool work perfectly "
+echo ""
+python modules/wpscanner.py -s http://$TARGET -n 20
+droopescan scan wordpress -u http://$TARGET
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                   Scan Wordpress Finished                      ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo ""
+echo ""
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                   Moving Metagoofil Files                      ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
+echo ""
+echo "Moving Metagoofil Folder"
+mv metagoofiles /root/Desktop 
+echo "Moving Metagoofil Folder Finished"
+echo ""
+echo "Moving Metagoofile data.html"
+mv data.html /root/Desktop
+echo "Moving Metagoofil data.html finished"
+echo ""
+echo ""
+echo ""
+echo ""
+echo "█[1]█"
+echo ""
+echo ""
+echo ""
+echo "█[2]█"
+echo ""
+echo ""
+echo ""
+echo "█[3]█"
+echo ""
+echo ""
+echo ""
+echo "╔════════════════════════════════════════════════════════════════╗"
+echo "║                    YUKI-CHAN FINISHED                          ║"
+echo "║                     HOPE YOU ENJOY IT                          ║"
+echo "║                       AND AS ALWAYS                            ║"
+echo "║                       HAVE NICE DAY                            ║"              
+echo "╚════════════════════════════════════════════════════════════════╝"
